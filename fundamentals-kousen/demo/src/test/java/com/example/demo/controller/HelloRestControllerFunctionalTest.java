a9173def5d590d26725409be9c4ebd1bafac45c2
Jim Newpower <jimnewpower@gmail.com>
2022-03-24 14:03:25 -0600

    Add functional test

---
+++ fundamentals-kousen/demo/src/test/java/com/example/demo/controller/HelloRestControllerFunctionalTest.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import com.example.demo.json.Greeting;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloRestControllerFunctionalTest {
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void greetWithName() {
+        Greeting response = template.getForObject("/rest?name=Russell", Greeting.class);
+        assertEquals("Hello, Russell!", response.message());
+    }
+
+    @Test
+    public void greetWithoutName() {
+        ResponseEntity<Greeting> entity = template.getForEntity("/rest", Greeting.class);
+        assertAll(
+                () -> assertEquals(HttpStatus.OK, entity.getStatusCode()),
+                () -> assertEquals(MediaType.APPLICATION_JSON, entity.getHeaders().getContentType())
+        );
+
+        Greeting response = entity.getBody();
+        assertAll(
+                () -> assertNotNull(response),
+                () -> assertEquals("Hello, World!", response.message())
+        );
+    }
+}
